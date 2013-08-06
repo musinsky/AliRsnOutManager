4903f491edc0eefa64e05dcff9f0338c4ad06f6a
Martin Vala <Martin.Vala@cern.ch>
2013-08-06 16:24:14 +0400

    added custom directory for eff in run.C macro

--- Sparse/run.C
+++ Sparse/run.C
@@ -2,9 +2,10 @@ void run(Int_t id=0,Int_t id_to=0, const char *path_Sparce_macro="") {
 
   gROOT->ProcessLine(TString::Format(".L %sSparse.C",path_Sparce_macro));
 
+  eff_prefix="EFFI_OK/effi_";
   del_step = 0.1;
   SetNameBordel(id, 30, 2010, 1);
   SetCombinations(1);
 
-  AnalyzeSparse();  
+  AnalyzeSparse();
 }
