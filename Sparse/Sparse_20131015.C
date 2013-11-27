0d3204bfa6f8ed36b5a4f9a0613e9cc4f6fd95b6
Martin Vala <Martin.Vala@cern.ch>
2013-11-27 23:21:13 +0100

    Fixed suf

--- Sparse/Sparse_20131015.C
+++ Sparse/Sparse_20131015.C
@@ -3,6 +3,7 @@
   rsn_data = 20131015;
   input_new = kTRUE;
   del_step = 0.20;
+  del_step = 0.50;
   mixing = kTRUE;
   //  del_step = 0.50; // Viktor
 
@@ -16,4 +17,28 @@
   AnalyzeSparse(kBlue);
   SetNameBordelNew(0, 40, 2010, kTRUE, "RsnOutput.root");
   AnalyzeSparse(kMagenta);
+
+  out_path=TString::Format("/eos/saske.sk/scratch/ALICE/RSN_OUT/2013-11-10/OUT/TPC%d/%d/",nsig,c);
+  if (!nsig) out_path.ReplaceAll("TPC0","NOPID");
+
+  // TPC 3Sigma
+  SetNameBordel(id, nsig, 2010, 1, in_fname.Data());
+  // NoPID
+  //SetNameBordel(id, 0, 2010, 1, in_fname.Data());
+  SetCombinations(c);
+
+  if (mixing) out_path+="MIX";
+  else out_path+="LS";
+  AnalyzeSparse();
+  if (!out_path.IsNull()) {
+    //gSystem->Exec(TString::Format("mkdir -p %s",out_path.Data()).Data());
+    //gSystem->Exec(TString::Format("mv *_PRIMARY_* %s/",out_path.Data()).Data());
+    TString out = gSystem->GetFromPipe("find -name *_PRIMARY_*");
+    out.ReplaceAll("./","");
+    Printf("xrdcp -R -f %s root://eos.saske.sk/%s/",out.Data(), out_path.Data());
+    gSystem->Exec(TString::Format("xrdcp -R -f %s root://eos.saske.sk/%s/%s/",out.Data(), out_path.Data(),out.Data()).Data());
+    gSystem->Exec(TString::Format("rm -rf %s",out.Data()).Data());
+  }
+
+
 }
