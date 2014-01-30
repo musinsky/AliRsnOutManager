c1acb89387496464f8c7916d673a2887610a9ab1
Jan Musinsky <musinsky@gmail.com>
2014-01-31 00:29:48 +0100

    add toLaTeX.C macro

---
+++ Sparse/toLaTeX.C
@@ -0,0 +1,58 @@
+// Jan Musinsky, Martin Vala
+// 2014-01-31
+
+Float_t x, y, dx, dy, dy2;
+ofstream outfile;
+
+void toLaTeX(const char *fname = "pt_RFE")
+{
+  TTree *t = new TTree();
+  t->ReadFile(fname, "x:y:dx:dy:dy2"); // default read as float
+
+  t->SetBranchAddress("x", &x);
+  t->SetBranchAddress("y", &y);
+  t->SetBranchAddress("dx", &dx);
+  t->SetBranchAddress("dy", &dy);
+  t->SetBranchAddress("dy2", &dy2);
+
+  outfile.open(Form("%s_TeX.tex", fname));
+  PrintHeader();
+
+  for (Int_t i = 0; i < t->GetEntries(); i++) {
+    t->GetEntry(i);
+    PrintRow();
+  }
+
+  PrintFooter();
+
+  outfile.close();
+  delete t;
+}
+
+void PrintHeader()
+{
+  outfile << "\\documentclass{article}" << endl;
+  outfile << "\\usepackage[table]{xcolor}\n" << endl;
+  outfile << "\\begin{document}" << endl;
+  outfile << "\\definecolor{grayKE}{gray}{0.75}" << endl;
+  outfile << "\\rowcolors{2}{grayKE}{white}\n" << endl;
+  outfile << "\\begin{tabular}{| c | c | c | c | c | c |}" << endl;
+  outfile << "  \\hline"<< endl;
+  outfile << "  $p_{T, min}$ & $p_{T, max}$ & value & stat. & sys. \\\\ \\hline"
+          << endl;
+  outfile << "  \\hline" << endl;
+}
+
+void PrintFooter()
+{
+  outfile << "\\end{tabular}" << endl;
+  outfile << "\n\\end{document}"<< endl;
+}
+
+void PrintRow()
+{
+  TString row;
+  row = Form("  %3.2f & %3.2f & %f & %f & %f \\\\ \\hline",
+             x - dx, x + dx, y, dy, dy2);
+  outfile << row.Data() << endl;
+}
